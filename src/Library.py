0ecd15e165a363da8ac0f63d849274144d32ea0b
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-12-01 00:28:00 +0300

    management operation completed

--- src/Library.py
+++ src/Library.py
@@ -27,7 +27,6 @@ class Kitap:
     def kitap_bilgisi(self):
         return f"Kitap: {self._ad},\n Yazarı: {self._yazar}, \n Fiyatı: {self._fiyat}"
 
-
 class Roman (Kitap):
     def __init__(self,ad,yazar,fiyat,tur):
         super().__init__(ad,yazar,fiyat)
@@ -35,7 +34,6 @@ class Roman (Kitap):
 
     def kitap_bilgisi(self):
         return f"Roman: {self.get_ad()}, Tür: {self.tur}, Yazar: {self.get_yazar()}, Fiyat: {self.get_fiyat()} TL"
-
 
 class DersKitabi(Kitap):
     def __init__(self, ad, yazar, fiyat, konu):
@@ -45,3 +43,45 @@ class DersKitabi(Kitap):
     # Polymorphism (Başka bir türde bilgi formatı)
     def kitap_bilgisi(self):
         return f"Ders Kitabı: {self.get_ad()}, Konu: {self.konu}, Yazar: {self.get_yazar()}, Fiyat: {self.get_fiyat()} TL"
+
+    
+class KitapYönetim:
+    def __init__(self):
+        self.kitaplar=[]
+
+    def kitap_ekle(self,kitap):
+        self.kitaplar.append(kitap)
+         print(f"{kitap.get_ad()} adlı kitap başarıyla eklendi.")
+
+    def kitap_listeleme(self):
+        if not self.kitaplar:
+            print("Sistemde kitap bulunmamaktadır.")
+        else:
+            for Kitap in self.kitaplar:
+                print(Kitap.kitap_bilgisi())
+
+    def kitap_silme(self,kitap_adi):
+        for kitap in self.kitaplar:
+            if kitap.get_ad() == kitap_adi:
+                self.kitaplar.remove(kitap)
+                 print(f"{kitap_adi} adlı kitap başarıyla silindi.")
+                return
+
+       print(f"{kitap_adi} adlı kitap bulunamadı.")
+
+# Tests
+yonetim = KitapYonetim()
+
+roman1 = Roman("Sefiller", "Victor Hugo", 45, "Klasik")
+ders_kitabi1 = DersKitabi("Matematik Analiz", "Ali Veli", 60, "Matematik")
+
+yonetim.kitap_ekle(roman1)
+yonetim.kitap_ekle(ders_kitabi1)
+
+print("\nKitap Listesi:")
+yonetim.kitaplari_listele()
+
+yonetim.kitap_sil("Sefiller")
+
+print("\nGüncel Kitap Listesi:")
+yonetim.kitaplari_listele()
